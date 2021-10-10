499aac062319ad8e132c2a6bfeebf16811ff9748
Luis Alvarado <luis0@outlook.com>
2021-10-09 22:00:44 -0500

    random commit

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+//console.log ("hola mundo"); // log es un método, como aprendí en C#, console es un objeto MDN buen lugar de documentación
+//console.error("error fatal"); // investigar duplicación de líneas
+
+//VARIABLES  var, let, const
+//let para reasignar valores, const no cambia
+
+
+// let age = 30;  /* así se debe inicializar una const, pero let deja solo declararla */
+// const dos = 2;
+//age = 22;
+
+//let score;
+
+//score=10;
+//score=14;
+//console.log (age, dos, score);
+//simepre usa const a menos que sepas que vas a reasignar valores
+
+//DATATYPES
+//strings, numbers, boolean, null, indefined, symbol PRIMITIVE DATATYPES (DATA ASIGGNED TO MEMORY)
+
+const nombre = 'Luis'
+const age = 30
+const rating = 4.5 //no floats in js
+const isCool = true
+const x = null;
+const y = undefined 
+let z; // same as undefined
+console.log(typeof age);
